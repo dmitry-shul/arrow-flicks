3d2a9bbc02a715f654e94b4b41f45745e52a9f54
Dmitry Shulzhytski <matemV@mail.ru>
2024-05-09 23:14:30 +0300

    refactor: proxy files

---
+++ middleware.js
@@ -0,0 +1,9 @@
+/*import { NextResponse } from 'next/server';
+
+export const config = {
+  matcher: '/movies',
+};
+ 
+export function middleware(request) {
+  return NextResponse.rewrite('/aaa');
+}*/
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  rewrites: async () => {
+    return [
+      {
+        source: '/movies',
+        destination: '/aaaa',
+      },
+    ];
+  },
+  /*rewrites: async () => {
+    return [
+      {
+        source: '/movies/:path*',
+        destination: 'https://api.themoviedb.org/3/discover/movies/:path*',
+        /*has: [
+          {
+            type: "host",
+            key: 'method',
+            value: 'GET',
+          },
+          {
+            type: "header",
+            key: 'accept',
+            value: 'application/json',
+          },
+          {
+            type: "header",
+            key: 'Authorization',
+            value: 'Bearer ' + process.env.API_KEY,
+          },
+        ],*/
+          
+      /*},
+    ];
+  },*/
 };
 
 export default nextConfig;
