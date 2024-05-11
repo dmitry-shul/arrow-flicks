48cdd025e4d1c262a38ab633826dd2576b048157
Dmitry Shulzhytski <matemV@mail.ru>
2024-05-11 23:29:28 +0300

    fix: store

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import filtersSlice from "./slices/filtersSlice"
-
-export const store = configureStore({
-  reducer: {
-    filters: filtersSlice,
-  },
-})
